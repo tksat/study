8306b1bd1dcb8917d8b7647ce5f1af791fc2bcac
大内智子 <jpgj5745@gmail.com>
2020-03-02 11:50:15 +0900

    useStateの実装

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React,{ useState } from 'react'
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0)
+  const incriment = () => setCount(count+1)
+  const decriment = () => setCount(count-1)
+  const reset = () => {setCount(0)}
+  const dubble = () => {setCount(count * 2)}
+  const threeDivision = () => count % 3 === 0 ? setCount(count/3) : count
+
   return (
-    <>テストでVimで記述して見ました！</>
+    <>
+      <p>count{count}</p>
+      <button onClick={incriment}>+</button>
+      <button onClick={decriment}>-</button>
+      <button onClick={dubble}>×2</button>
+      <button onClick={reset}>reset</button>
+      <button onClick={threeDivision}>3の倍数の時だけ3で割る</button>
+    </>
   );
 }
 
