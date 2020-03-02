89cdf2a56c1e6976ae644a8384058f5f12ef131b
大内智子 <jpgj5745@gmail.com>
2020-03-02 12:37:04 +0900

    resetとstateのリファクタリング

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -2,17 +2,13 @@ import React,{ useState } from 'react'
 
 const App = props => {
   const [state, setState] = useState(props)
-
-  const reset = () => {
-    setState(props)
-  }
-
+  const {name, price} = state
   return (
     <>
-      <h2>{`${state.name}は${state.price}円です`}</h2>
-      <input onChange={e => setState({...state, name: e.target.value})} value={state.name} />
-      <button onClick={() => setState({...state, price: state.price + 100})}>+100</button>
-      <button onClick={reset}>リセット</button>
+      <h2>{`${name}は${price}円です`}</h2>
+      <input onChange={e => setState({...state, name: e.target.value})} value={name} />
+      <button onClick={() => setState({...state, price: price + 100})}>+100</button>
+      <button onClick={() => setState(props)}>リセット</button>
     </>
   )
 }
