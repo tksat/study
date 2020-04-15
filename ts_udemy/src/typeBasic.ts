280e86151399a5245dd7c3fdf4f76acff6b5caae
大内智子 <jpgj5745@gmail.com>
2020-04-15 11:16:33 +0900

    type basic

--- ts_udemy/src/typeBasic.ts
+++ ts_udemy/src/typeBasic.ts
@@ -31,7 +31,7 @@ let nijigenHairestu: number[][] = [
 ]
 let nijigenHairestu2: (string | number | boolean)[] = [1, false, 'japan']
 
-//tuple型 オブジェクト順番に合わせて型を固定したい場合
+//tuple型 オブジェクトが固定された型の場合
 let myData: [string, number] = ['sat', 37]
 console.log(myData)
 
@@ -43,20 +43,55 @@ console.log(returnNothing())
 
 //never型 例外を返す物に設定（エラー型とか）
 //void型との違いは、呼び元に戻ってこない事
-function error(message: string): never{
-  throw new Error(message)
-}
+// function error(message: string): never{
+//   throw new Error(message)
+// }
 
-try{
-  let result = error('エラーです')
-  console.log({ result })
-}catch(error){
-  console.log({ error })
-}
+// try{
+//   let result = error('エラーです')
+//   console.log({ result })
+// }catch(error){
+//   console.log({ error })
+// }
 
 //null型 undefined型（基本使用しない）
 let absence: null = null
 let data: undefined = undefined
 
 //object型
-let profile = {name: 'sat'}
+let profile: { name: string } = {name: 'sat'}
+
+//型エイリアス
+type Moji = string;
+const fooString: string = "hello"
+const moji: Moji = "moji" 
+
+type profile = {
+  name: string
+  age: number
+}
+
+const user: profile = {
+  name: "sato",
+  age: 30
+}
+
+const profile2 = {
+  name: "satoko",
+  age: 37
+}
+
+//string等で指定しなくても自動で型推論してくれる
+type user2 = typeof profile2
+
+//interface
+//typeよりすっきり書ける
+interface objectType{
+  name: string,
+  age: number
+}
+
+const profile3: objectType = {
+  name: "sato",
+  age: 30
+}
