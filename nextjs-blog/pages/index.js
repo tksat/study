6cd8d64c5c1da39e208dfc0e8b0f36585b3be160
大内智子 <jpgj5745@gmail.com>
2020-05-13 15:37:41 +0900

    NEXTチャプター4の学習完了

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,9 +1,9 @@
 import Head from "next/head"
 import Layout, { siteTitle } from "../components/layout"
 import utilStyles from "../styles/utils.module.css"
-import Alert from "../components/Alert"
+import { getSortedPostsData } from "../lib/posts"
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout>
       <Head>
@@ -17,9 +17,24 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-      <Alert type="success">
-        <p>classnamesのサンプルです</p>
-      </Alert>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, data, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <h3>{title}</h3>
+              <p>{data}</p>
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: { allPostsData },
+  }
 }
